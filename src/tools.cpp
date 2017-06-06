c20d4d2e8a6dd143b5d43934c8dd9f6d12fbd37d
Pradeep Banavara <pradeepbs@gmail.com>
2017-06-06 10:07:37 +0100

    Code review fixes
    FusionEKF.cpp - Initialized the H_Lazer matrix
    tools.cpp - Changed the division by 0 logic

--- src/tools.cpp
+++ src/tools.cpp
@@ -60,15 +60,12 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     float vy = x_state(3);
 
     //pre-compute a set of terms to avoid repeated calculation
-    float c1 = px*px+py*py;
+    float eps = 0.0001;
+    const double c1 = std::max(eps, px*px + py*py);
     float c2 = sqrt(c1);
     float c3 = (c1*c2);
 
     //check division by zero
-    if(fabs(c1) < 0.0001){
-        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
-        return Hj;
-    }
 
     //compute the Jacobian matrix
     Hj << (px/c2), (py/c2), 0, 0,
